cd520fe2d7ffb171c1158875770e2f4a49445264
Rob Hess <hessro@oregonstate.edu>
2022-11-22 15:46:37 -0800

    Add code from 11/22 lecture.

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require('express');
+var exphbs = require('express-handlebars')
+
+var peopleData = require("./peopleData.json")
+console.log("== peopleData:", peopleData)
 
 var port = process.env.PORT || 8000;
 var app = express();
+
+app.engine('handlebars', exphbs.engine({
+  defaultLayout: "main"
+}))
+app.set('view engine', 'handlebars')
 
 app.use(express.static('public'));
 
@@ -9,27 +18,25 @@ app.get('/people', function (req, res, next) {
   res.status(200).sendFile(__dirname + '/public/people.html');
 });
 
-var availablePeople = [
-  'luke',
-  'leia',
-  'rey',
-  'finn',
-  'r2d2'
-];
 
 app.get('/people/:person', function (req, res, next) {
   var person = req.params.person.toLowerCase();
-  if (availablePeople.indexOf(person) >= 0) {
-    res.status(200).sendFile(
-      __dirname + '/public/people/' + person + '.html'
-    );
+  var personData = peopleData[person]
+  console.log("== data for", person, ":", personData)
+
+  if (personData) {
+    res.status(200).render('photoPage', {
+      name: personData.name,
+      photos: personData.photos,
+      // truthyOrFalsy: null
+    })
   } else {
     next();
   }
 });
 
 app.get("*", function (req, res, next) {
-  res.status(404).sendFile(__dirname + '/public/404.html');
+  res.status(404).render('404')
 });
 
 app.listen(port, function (err) {
